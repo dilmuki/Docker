53d49a1263b28a2d9eb9f058500b3dd0772ed049
farrukh90 <farrukhsadykov@gmail.com>
2018-07-25 09:42:41 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,3 +6,6 @@ yum-config-manager --disable docker-ce-edge
 yum install docker-ce -y
 systemctl start docker
 echo "Docker is running " 
+
+read -p "Please enter IP address: " IP
+docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.4 install --host-address $IP --interactive
